8b1ea2f4f0556d0aa156fa2d8441d2b22a616ef3
Juan David Orejuela Bolaños <orejuelajd@gmail.com>
2015-10-17 16:46:48 -0500

    Prueba nicolas

--- urbanraspberry-arduino.ino
+++ urbanraspberry-arduino.ino
@@ -6,7 +6,7 @@ HttpClient client;
 int variable_id = 0;
 int value = 0;
 char c;
-StaticJsonBuffer<200> jsonBuffer;
+StaticJsonBuffer<1000> jsonBuffer;
 char cadena[40];
 
 /*
@@ -15,58 +15,74 @@ char cadena[40];
  * con el id del dispositivo agregado
  * en la interfaz gráfica de la Raspberry
 */
-String ARDUINOID="arduinoyunuao";
+String ARDUINOID = "arduinoyunuao";
 
 void setup() {
 
   pinMode(13, OUTPUT);
   digitalWrite(13, LOW);
   Bridge.begin();
-  
+
   //Se prende la luz roja del Yun (led 13)
   //como señal para saber que inició Bridge
   digitalWrite(13, HIGH);
 
-  
+
   Serial.begin(9600);
   while (!Serial);
 
   //Se llama al servicio web de la Raspberry, pidiendo
   //la lista de equipos disponibles
-  client.get("http://192.168.1.10/urbanraspberry/equipos");
- 
+  client.get("http://192.168.1.10/urbanraspberry/equipos/"+ARDUINOID);
+
   String equiposText = "";
+  //String slashed="";
   while (client.available()) {
     char c = client.read();
+    if(c=='"'){
+      //slashed+="\\";
+    }
     equiposText += c;
+    //slashed+=c;
   }
 
-  Serial.println(equiposText);
 
-  JsonArray& equipos = jsonBuffer.parseArray(equiposText);
-  Serial.println(equipos.toString());
-  for(JsonArray::iterator it=equipos.begin(); it!=equipos.end(); ++it) 
+  JsonObject& equipos = jsonBuffer.parseObject(equiposText);
+
+  if (!equipos.success())
+  {
+    Serial.println("parseObject() failed");
+  }else{
+    Serial.println("succes!!");
+  }
+
+
+  JsonArray& ar = equipos[0]["ar"];
+  Serial.println((const char *) equipos[0]["ar"][0]["hola"]);
+  /*for(JsonArray::iterator it=equipos.begin(); it!=equipos.end(); ++it)
   {
       // *it contains the JsonVariant which can be casted as usuals
       const char* sensor = (*it)["id"];
       Serial.println(sensor);
-  
-      // this also works: 
-      //value = it->as<const char*>();    
-  
-  }
+
+      // this also works:
+      //value = it->as<const char*>();
+
+  }*/
 }
 
 
 
 void loop() {
-  
- /* variable_id = equipos["vari"];
-  value = equipos["pines"];
-  Serial.print(value);
 
-  */
+  /* variable_id = equipos["vari"];
+   value = equipos["pines"];
+   Serial.print(value);
+
+   */
 
   Serial.flush();
   delay(2000);
+
+  //prueba nicolas
 }
